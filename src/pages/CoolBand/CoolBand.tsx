9955c1dc2d56cfd0cdcde061f70e21530210f8b1
Maxim <334890@gmail.com>
2023-12-29 22:10:36 +0600

    fix import & remove main>div

--- src/pages/CoolBand/CoolBand.tsx
+++ src/pages/CoolBand/CoolBand.tsx
@@ -11,22 +11,27 @@ import styles from "../../scss/partials/coolBand/_gis2.module.scss";
 const CoolBand = () => {
    return (
       <>
-         <div>
-            <Hero />
-            <CoolShowBand />
-            <WhyUs />
-            <OurEvents />
-            <OurTeam />
-            <Collaboration/>
-            <OurClients />
-            <Gis2 callToUs={"Связаться с нами"}
-                  music={"Не теряй связь с музыкой!"}
-                  toBook={"Чтобы забронировать выступление Coolshowband" +
-                     " отправьте нам  сообщение на WhatsApp. Мы свяжемся" +
-                     " с вами в ближайшее время и ответим на все интересующие вас вопросы"}
-                  btn={<a href="tel:996755608908" className={`${styles.btnCall}`}>Связаться с нами</a>}
-            />
-         </div>
+         <Hero />
+         <CoolShowBand />
+         <WhyUs />
+         <OurEvents />
+         <OurTeam />
+         <Collaboration />
+         <OurClients />
+         <Gis2
+            callToUs={"Связаться с нами"}
+            music={"Не теряй связь с музыкой!"}
+            toBook={
+               "Чтобы забронировать выступление Coolshowband" +
+               " отправьте нам  сообщение на WhatsApp. Мы свяжемся" +
+               " с вами в ближайшее время и ответим на все интересующие вас вопросы"
+            }
+            btn={
+               <a href="tel:996755608908" className={`${styles.btnCall}`}>
+                  Связаться с нами
+               </a>
+            }
+         />
       </>
    );
 };
