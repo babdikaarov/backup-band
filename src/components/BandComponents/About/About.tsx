08864802bd8257789f62bbb46b4e22902d30a270
Beksultan A <babdikaarov@gmail.com>
2024-01-05 20:00:52 +0300

    about band restore static

--- src/components/BandComponents/About/About.tsx
+++ src/components/BandComponents/About/About.tsx
@@ -1,21 +1,8 @@
-import { useEffect, useState } from "react";
 import AboutTemplate from "../../../UI/About/AboutTemplate";
-import getAboutData from "../../../tempData/getAboutData";
-type AboutData = {
-   text: string;
-   poster: Promise<typeof import("*.jpg")>;
-};
+import image from "/src/assets/about/about.jpg";
+import text from "./text.json";
+
 const About = () => {
-   const [data, setData] = useState<AboutData | undefined>();
-
-   useEffect(() => {
-      const loadImages = async () => {
-         await new Promise(getAboutData).then((imported) => setData(imported.aboutBand));
-      };
-
-      loadImages();
-   }, []);
-
-   return <AboutTemplate header="Cool Show band" data={data} />;
+  return <AboutTemplate header="Cool Show band" text={text} poster={image} />;
 };
 export default About;
